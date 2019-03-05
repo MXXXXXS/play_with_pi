c093fabca92af6a8061a928221c9c716576cd072
mxxxxxs <relife_abyss@qq.com>
2019-03-05 22:02:55 +0800

    websocket连通了

---
+++ controler/controler.js
@@ -0,0 +1,10 @@
+const pi = 'ws://127.0.0.1:2333'
+const ws = new WebSocket(pi)
+ws.onopen = e => {
+  console.log('linked')
+  ws.send('hello, pi')
+}
+
+ws.onmessage = e => {
+  console.log('receieved: ' + e.data)
+}
--- pi/server.js
+++ pi/server.js
@@ -4,8 +4,16 @@ const fs = require('fs');
 const formidable = require('formidable');
 const WS = require('ws')
 const wss = new WS.Server({
-  host: 'pi',
-  port: 2333,
+  port: 2333
+})
 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(msg) {
+    console.log('received: ' + msg)
+  })
+  ws.on('error', function (e) {
+    console.error(e)
+  })
+  ws.send('hi, from ws')
 })
 
